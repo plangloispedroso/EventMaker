8ecb12cecd0aff0cef4a83fa6e06839ddaa6031f
Philippe Langlois <planglois.pedroso@gmail.com>
2018-02-11 15:15:32 -0500

    added functionality for sending a message to a number

--- app/src/main/java/com/parm/eventmaker/SendToContactsActivity.java
+++ app/src/main/java/com/parm/eventmaker/SendToContactsActivity.java
@@ -1,8 +1,12 @@
 package com.parm.eventmaker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +24,7 @@ public class SendToContactsActivity extends Activity{
     Context context;
     Button sendButton;
     EditText numberEdit, smsEdit;
+    String phoneNo, sms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,31 +35,63 @@ public class SendToContactsActivity extends Activity{
         sendButton = (Button) findViewById(R.id.send_contact_send_button);
         numberEdit = (EditText) findViewById(R.id.send_contact_phone_edit);
         smsEdit = (EditText) findViewById(R.id.send_contact_sms_edit);
+        phoneNo = numberEdit.getText().toString();
+        sms = smsEdit.getText().toString();
+    }
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
+    /**
+     *
+     * @param view
+     */
+    public void checkPermissions(View view){
+        if(ContextCompat.checkSelfPermission(context,
+                Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+            sendMessage();
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS}, 130);
+        }
+    }
 
-            @Override
-            public void onClick(View v) {
-
-                String phoneNumber = numberEdit.getText().toString();
-                String sms = smsEdit.getText().toString();
-                double phoneNum = 0;
-
-                try {
-                    phoneNum = Double.parseDouble(phoneNumber);
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNumber, null, sms, null, null);
-                    Toast.makeText(context, getResources().getString(R.string.success_sending_sms),
-                            Toast.LENGTH_LONG).show();
-                }catch(NumberFormatException nfe){
-                    Toast.makeText(context, getResources().getString(R.string.error_invalid_phone_number),
-                            Toast.LENGTH_LONG).show();
-                }catch(Exception e){
-                    Toast.makeText(context, getResources().getString(R.string.error_sending_sms),
-                            Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
+    /**
+     * Check if the user has SENS_SMS permission set to allow. If allowed, send a message. If
+     * the user has not allowed do nothing.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 130: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendMessage();
                 }
+                return;
             }
-        });
+        } // end of switch
+    }
+
+    /**
+     *
+     */
+    public void sendMessage(){
+
+        String number = numberEdit.getText().toString();
+        String message = smsEdit.getText().toString();
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(number, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
