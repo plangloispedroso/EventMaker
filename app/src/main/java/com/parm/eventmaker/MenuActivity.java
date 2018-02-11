8de9869854275333c6595daf33e73ffc08a5f5b3
student <student@ITSA35811E-11.marianopolis.com>
2018-02-11 12:57:16 -0500

    menu activity done. Just need to make all activities extend it

---
+++ app/src/main/java/com/parm/eventmaker/MenuActivity.java
@@ -0,0 +1,43 @@
+package com.parm.eventmaker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case MENU_ABOUT:
+                showAbout();
+                return true;
+            case MENU_SEARCH:
+                showSearch();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAbout(){
+        Intent abt = new Intent();
+
+        startActivity(abt);
+    }
+
+    private void showSearch(){
+        Intent srch = new Intent();
+
+        startActivity(srch);
+    }
+
+}
